2568ef9bd3c6a1590869d114bc69531400138c42
JRaghu842 <jraghavendra842@gmail.com>
2024-04-06 18:04:56 +0530

    Diffusion vaults section icons updated

--- landingpage/src/components/TopVaults.jsx
+++ landingpage/src/components/TopVaults.jsx
@@ -3,7 +3,11 @@ import { useRef, useEffect } from "react";
 import "./TopVaults.css"
 
 import BGRing from "../assets/BGRing.png";
-import icon1 from "../assets/ico1.png";
+import vault1 from "../assets/vault1.png";
+import vault2 from "../assets/vault2.png";
+import vault3 from "../assets/vault3.png";
+import vault4 from "../assets/vault4.png";
+
 
 
 function TopVaults() {
@@ -449,17 +453,17 @@ function TopVaults() {
 
         <div className="w-[60%] flex flex-col justify-between items-center">
 
-          <div className="p-5 w-full rounded-3xl flex justify-between items-center bg-[#020916]">
+          <div className="p-5 w-full h-[240px] rounded-3xl flex justify-between items-center bg-[#020916]">
 
             <div className="w-[48%] flex flex-col justify-between items-start p-1">
-              <img src={icon1} alt="err" className="w-[17%] mr-4 rounded-full"/>
+              <img src={vault1} alt="err" className="w-[13%] ml-2 rounded-full"/>
               <p className="font-light text-base text-white leading-6 mt-4">Integration with plugins and automation to enable easy creation and management of strategies</p>
             </div>
 
-            <div className="h-[120px] border"></div>
+            <div className="h-[180px] border"></div>
 
-            <div className="w-[48%] flex flex-col justify-between items-start  ml-2">
-            <img src={icon1} alt="err" className="w-[17%] mr-4 rounded-full"/>
+            <div className="w-[48%] flex flex-col justify-between items-start  ml-4">
+            <img src={vault2} alt="err" className="w-[10%] ml-2 rounded-full"/>
               <p className="font-light text-base text-white leading-6 mt-4">Enable custom share minting and performance fee to extend your stratgies to public</p>
             </div>
           </div>
@@ -469,14 +473,14 @@ function TopVaults() {
           <div className="p-5 w-full rounded-3xl flex justify-between items-center bg-[#020916] mt-6">
 
             <div className="w-[48%] flex flex-col justify-between items-start p-1 ">
-              <img src={icon1} alt="err" className="w-[17%] mr-4 rounded-full"/>
+              <img src={vault3} alt="err" className="w-[20%] ml-2 rounded-full"/>
               <p className="font-light text-base text-white leading-6 mt-4">If you always thought of using Uniswap and AAVE together at one place and putting idle assets in AAVE till your buying order gets triggered this is what you need</p>
             </div>
 
             <div className="h-[180px] border"></div>
 
-            <div className="w-[48%] flex flex-col h-[180px]  items-baseline  ml-2 ">
-            <img src={icon1} alt="err" className="w-[17%] mr-4 rounded-full"/>
+            <div className="w-[48%] flex flex-col h-[180px]  items-baseline  ml-4 ">
+            <img src={vault4} alt="err" className="w-[18%] mr-4 rounded-full"/>
               <p className="font-light text-base text-white leading-6 mt-4">It gives you access to create custom logic and automated strategy</p>
             </div>
           </div>
