8a241eb020d59ebbda93097238a1658602f45963
JRaghu842 <jraghavendra842@gmail.com>
2024-04-10 12:53:47 +0530

    UI updated

--- landingpage/src/components/Community.jsx
+++ landingpage/src/components/Community.jsx
@@ -168,7 +168,7 @@ function Community() {
         Don't just follow the market - lead it. Explore and employ top-performing strategies curated by our community of DeFi experts and trailblazers.
         </div>
 
-        <div className="mt-8  rounded-xl card-wrapper">
+        <div className="mt-8  rounded-xl card-wrapper custom-shadow">
 
         <div className="card-content p-[52px] mb-[6px] ml-[1px] -mt-[2px] rounded-xl bg-black ">
 
